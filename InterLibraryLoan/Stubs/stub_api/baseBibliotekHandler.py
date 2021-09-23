0c2ef9bcb2b3b3c07a4fe3b4b17756a5b40cb21e
anetteOlli <ansi@unit.no>
2021-09-23 16:56:59 +0200

    added prefix stub to avoid api collisions, also basebibliotek now returns mock ncip url

--- InterLibraryLoan/Stubs/stub_api/baseBibliotekHandler.py
+++ InterLibraryLoan/Stubs/stub_api/baseBibliotekHandler.py
@@ -1,6 +1,9 @@
+
 def handler_basebibliotek_sender(event, context):
     f = open("/var/task/resources/baseBibliotekWithLineEndingInInst.xml", "r")
     pnx_example = f.read()
+    # The ncip url is changed here programatically for maintenance reasons.
+    pnx_example = pnx_example.replace("https://bibsys.alma.exlibrisgroup.com/view/NcipP2PServlet", "https://api.test.bibs.aws.unit.no/stub/ncip")
     return {
         "statusCode": 200, "headers": {
             "Content-Type": "text/xml"
