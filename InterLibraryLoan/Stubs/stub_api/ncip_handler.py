684876f1b7d46a6a8939585be4ca88fd430ee23d
anetteOlli <ansi@unit.no>
2021-09-24 15:47:47 +0200

    testing import

--- InterLibraryLoan/Stubs/stub_api/ncip_handler.py
+++ InterLibraryLoan/Stubs/stub_api/ncip_handler.py
@@ -3,11 +3,13 @@ import xmlschema
 
 def handler_pnx_sender(event, context):
     print("NCIP HANDLER EVENT: ", event)
-    print("NCIP HANDLER EVENT BODY", event.body)
+    #  print("NCIP HANDLER EVENT BODY", event.body)
+
 
     xsd_file = open("/var/task/resources/ncip_v2_02.xsd", "r")
     ncip_xsd = xsd_file.read()
     xsd = xmlschema.XMLSchema(ncip_xsd)
+    """
     if not xsd.is_valid(event.body):
         failure_xml_response_file = open("/var/task/resources/ncipResponseFailure.xml", "r")
         failure_xml_response = failure_xml_response_file.read()
@@ -17,6 +19,7 @@ def handler_pnx_sender(event, context):
             },
             "body": failure_xml_response
         }
+    """
     xml_file = open("/var/task/resources/ItemRequestedResponseSuccess.xml", "r")
     success_item_request_response = xml_file.read()
 
