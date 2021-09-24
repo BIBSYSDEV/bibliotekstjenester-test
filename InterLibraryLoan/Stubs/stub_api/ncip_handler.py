ffb562ef1c6b1334de76ad5d10c78ff302c80321
anetteOlli <ansi@unit.no>
2021-09-24 12:25:40 +0200

    testing simple response from ncip

--- InterLibraryLoan/Stubs/stub_api/ncip_handler.py
+++ InterLibraryLoan/Stubs/stub_api/ncip_handler.py
@@ -1,10 +1,10 @@
 def handler_pnx_sender(event, context):
     f = open("/var/task/resources/ItemRequestResponseSuccess.xml", "r")
     success_item_request_response = f.read()
-    print("NCIP REQUEST: ", event)
+
     return {
         "statusCode": 202, "headers": {
             "Content-Type": "text/xml"
         },
-        "body": {success_item_request_response, event}
+        "body": success_item_request_response
     }
