62ec9390f490ec869fa9211b5e3b6d38ef1cdf60
anetteOlli <ansi@unit.no>
2021-09-24 16:11:52 +0200

    validating event body

--- InterLibraryLoan/Stubs/stub_api/ncip_handler.py
+++ InterLibraryLoan/Stubs/stub_api/ncip_handler.py
@@ -3,14 +3,19 @@ import xmlschema
 
 def handler_pnx_sender(event, _context):
     print("NCIP HANDLER EVENT: ", event)
-    print("NCIP HANDLER EVENT BODY", event['body'])
-
     xsd_file = open("/var/task/resources/ncip_v2_02.xsd", "r")
     ncip_xsd = xsd_file.read()
     xsd = xmlschema.XMLSchema(ncip_xsd)
 
-    """
-    if not xsd.is_valid(event.body):
+    if event['body'] is None:
+        return {
+            "statusCode": 400, "headers": {
+                "Content-Type": "application/xml"
+            },
+            "body": "bad request"
+        }
+
+    if not xsd.is_valid(event['body']):
         failure_xml_response_file = open("/var/task/resources/ncipResponseFailure.xml", "r")
         failure_xml_response = failure_xml_response_file.read()
         return {
@@ -19,7 +24,7 @@ def handler_pnx_sender(event, _context):
             },
             "body": failure_xml_response
         }
-    """
+
     xml_file = open("/var/task/resources/ItemRequestedResponseSuccess.xml", "r")
     success_item_request_response = xml_file.read()
 
