d045e56b359e66a5feaba32a557214f95278c0ad
anetteOlli <ansi@unit.no>
2021-09-24 13:24:35 +0200

    change in file name

--- InterLibraryLoan/Stubs/stub_api/ncip_handler.py
+++ InterLibraryLoan/Stubs/stub_api/ncip_handler.py
@@ -1,6 +1,7 @@
 def handler_pnx_sender(event, context):
     f = open("/var/task/resources/ItemRequestedResponseSuccess.xml", "r")
     success_item_request_response = f.read()
+    print("NCIP HANDLER EVENT: ", event)
 
     return {
         "statusCode": 202, "headers": {
