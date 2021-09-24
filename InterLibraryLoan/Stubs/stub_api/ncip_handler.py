37fe02e7879ec539674bce54d6c519ecce835409
anetteOlli <ansi@unit.no>
2021-09-24 15:27:28 +0200

    added code for validation ncip request body

--- InterLibraryLoan/Stubs/stub_api/ncip_handler.py
+++ InterLibraryLoan/Stubs/stub_api/ncip_handler.py
@@ -1,11 +1,28 @@
+import xmlschema
+
+
 def handler_pnx_sender(event, context):
-    f = open("/var/task/resources/ItemRequestedResponseSuccess.xml", "r")
-    success_item_request_response = f.read()
     print("NCIP HANDLER EVENT: ", event)
+    print("NCIP HANDLER EVENT BODY", event.body)
+
+    xsd_file = open("/var/task/resources/ncip_v2_02.xsd", "r")
+    ncip_xsd = xsd_file.read()
+    xsd = xmlschema.XMLSchema(ncip_xsd)
+    if not xsd.is_valid(event.body):
+        failure_xml_response_file = open("/var/task/resources/ncipResponseFailure.xml", "r")
+        failure_xml_response = failure_xml_response_file.read()
+        return {
+            "statusCode": 400, "headers": {
+                "Content-Type": "application/xml"
+            },
+            "body": failure_xml_response
+        }
+    xml_file = open("/var/task/resources/ItemRequestedResponseSuccess.xml", "r")
+    success_item_request_response = xml_file.read()
 
     return {
         "statusCode": 202, "headers": {
-            "Content-Type": "text/xml"
+            "Content-Type": "application/xml"
         },
         "body": success_item_request_response
     }
