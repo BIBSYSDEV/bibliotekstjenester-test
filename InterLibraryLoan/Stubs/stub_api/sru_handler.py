6cc3b87b314fccd993cc3ce47a0bc4941a51e7dd
anetteOlli <ansi@unit.no>
2021-09-29 10:05:33 +0200

    SMILE-950-mock-response, added sru endpoint

---
+++ InterLibraryLoan/Stubs/stub_api/sru_handler.py
@@ -0,0 +1,28 @@
+from InterLibraryLoan.Stubs.stub_api.constants_ids import SRU_SRU_NO_AVAILABLE_BOOKS
+
+
+def sru_handler(event, _context):
+    query = event['queryStringParameters']
+    institution = event['institution']
+    mms_id = query['mms_id']
+    library_code = query['libraryCode']
+    total_items = 2
+    number_available_for_inter_library_loan = 1
+
+    if SRU_SRU_NO_AVAILABLE_BOOKS == library_code:
+        number_available_for_inter_library_loan = 0
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        "body": {
+            "mmsId": mms_id,
+            "institution": institution,
+            "libraryCode": library_code,
+            "totalNumberOfItems": total_items,
+            "numberAvailForInterLibraryLoan": number_available_for_inter_library_loan,
+            "availableDate": "2021-09-08T00:00Z"
+        }
+    }
