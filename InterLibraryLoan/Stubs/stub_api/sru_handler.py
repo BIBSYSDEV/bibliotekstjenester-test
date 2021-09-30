c0843b45e1c3ce0e8450e88dc6b8c979f86eacbb
anetteOlli <ansi@unit.no>
2021-09-30 16:07:59 +0200

    corrected filepath to sru stubb

--- InterLibraryLoan/Stubs/stub_api/sru_handler.py
+++ InterLibraryLoan/Stubs/stub_api/sru_handler.py
@@ -3,7 +3,7 @@ from stub_api.constants_ids import FAILURE_LIBRARY, CLOSED_LIBRARY
 
 def sru_handler(_event, _context):
 
-    f = open("/var/task/resources/sru_holding_with_only_one_copy.xml", "r")
+    f = open("/var/task/resources/sru_holdings_with_only_copy.xml", "r")
     sru_response_1_hit = f.read()
 
     return {
