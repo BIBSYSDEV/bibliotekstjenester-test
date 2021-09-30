32d48e8cb7603bbc1663c4d97eb46bef9c2b04c4
anetteOlli <ansi@unit.no>
2021-09-30 15:50:05 +0200

    changed sru stubbing

--- InterLibraryLoan/Stubs/stub_api/sru_handler.py
+++ InterLibraryLoan/Stubs/stub_api/sru_handler.py
@@ -1,31 +1,15 @@
 from stub_api.constants_ids import FAILURE_LIBRARY, CLOSED_LIBRARY
-import json
 
 
-def sru_handler(event, _context):
-    query = event['queryStringParameters']
-    institution = query['institution']
-    mms_id = query['mms_id']
-    library_code = query['libraryCode']
-    total_items = 2
-    number_available_for_inter_library_loan = 1
+def sru_handler(_event, _context):
 
-    if FAILURE_LIBRARY == library_code or CLOSED_LIBRARY == library_code:
-        number_available_for_inter_library_loan = 0
-
-    body = {
-        "mmsId": mms_id,
-        "institution": institution,
-        "libraryCode": library_code,
-        "totalNumberOfItems": total_items,
-        "numberAvailForInterLibraryLoan": number_available_for_inter_library_loan,
-        "availableDate": "2021-09-08T00:00Z"
-    }
+    f = open("/var/task/resources/sru_holding_with_only_one_copy.xml", "r")
+    sru_response_1_hit = f.read()
 
     return {
         "statusCode": 200,
         "headers": {
-            "Content-Type": "application/json; charset=utf-8"
+            "Content-Type": "application/xml; charset=utf-8"
         },
-        "body": json.dumps(body)
+        "body": sru_response_1_hit
     }
