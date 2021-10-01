9424e33e186696e450c9477e6a05e9074b65c630
anetteOlli <ansi@unit.no>
2021-10-01 10:21:16 +0200

    added extra sru response

--- InterLibraryLoan/Stubs/stub_api/sru_handler.py
+++ InterLibraryLoan/Stubs/stub_api/sru_handler.py
@@ -1,7 +1,19 @@
 from stub_api.constants_ids import FAILURE_LIBRARY, CLOSED_LIBRARY
 
 
-def sru_handler(_event, _context):
+def sru_handler(event, _context):
+    query = event['queryStringParameters']['query']
+
+    if "999919860233902214" in query:
+        f = open("/var/task/resources/sru_holdings_complex.xml", "r")
+        sru_holdings_complex = f.read()
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/xml; charset=utf-8"
+            },
+            "body": sru_holdings_complex
+        }
 
     f = open("/var/task/resources/sru_holdings_with_only_copy.xml", "r")
     sru_response_1_hit = f.read()
