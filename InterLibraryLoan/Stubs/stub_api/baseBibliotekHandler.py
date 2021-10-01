c1cab8e27879ea052486def25eaedd750216b7ad
anetteOlli <ansi@unit.no>
2021-10-01 14:58:30 +0200

    added server response that should be unexpected for libcheck and metadata handler

--- InterLibraryLoan/Stubs/stub_api/baseBibliotekHandler.py
+++ InterLibraryLoan/Stubs/stub_api/baseBibliotekHandler.py
@@ -1,11 +1,20 @@
 from stub_api.constants_ids import CLOSED_LIBRARY, NCIP_SUCCESS_ID, FAILURE_LIBRARY, \
-    NCIP_FAILURE_ID, NCIP_ONLY_LIBRARY, ALMA_ONLY_LIBRARY, NEITHER_ALMA_NOR_NCIP_LIBRARY, ALMA_AND_NCIP_LIBRARY
+    NCIP_FAILURE_ID, NCIP_ONLY_LIBRARY, ALMA_ONLY_LIBRARY, NEITHER_ALMA_NOR_NCIP_LIBRARY, ALMA_AND_NCIP_LIBRARY, \
+    SERVER_CRASH_LIBRARY
 
 
 def handler_basebibliotek_sender(event, _context):
     # api gateway already checks if identifier exists in the template.yml file
     # so no need to check if base_bibliotek_identifier is None
     base_bibliotek_identifier = event['pathParameters']['identifier']
+
+    if SERVER_CRASH_LIBRARY == base_bibliotek_identifier:
+        return {
+            "statusCode": 200, "headers": {
+                "Content-Type": "application/xml"
+            },
+            "body": "GURBA!"
+        }
 
     # CLOSED LIBRARY
     if CLOSED_LIBRARY == base_bibliotek_identifier:
