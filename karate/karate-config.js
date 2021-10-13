a68abc610d4f511649068d0487a528924e5f78fc
Gregor Gabriel <gregor.gabriel@unit.no>
2021-10-13 14:32:02 +0200

    SMILE-1001: karate.jar java kjøring

--- karate/karate-config.js
+++ karate/karate-config.js
@@ -1,5 +1,5 @@
 function fn() {
-    let apiKey = java.lang.System.getenv("HOME");
+    var apiKey = java.lang.System.getenv("HOME");
     karate.log(apiKey);
     return {
         ALMA_APIKEY: apiKey,
